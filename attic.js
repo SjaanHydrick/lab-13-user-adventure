38716ddb7571809f261935c5cf7ea43179f39dd5
Sjaan Hydrick <sjaanahydrick@gmail.com>
2020-10-16 21:33:07 -0700

    quest display

--- attic.js
+++ attic.js
@@ -12,7 +12,7 @@ const subway = {
 
     sound:'',
 
-    description: `You descend into the dimly lit cave, moving slowly to give your eyes a change to adjust to the darkness. The walls are lined with slick stones arranged in rectangle patterns. They feel cool against your fingertips. A large crevice in the middle of the cave is dominated by a twisted metal structure that emerges from a tunnel. An ancient home, perhaps? Or was it what the elders called "automobiles"? Whatever its name, it could contain a relic. There is a large pile of rubble to the north where part of the ceiling has collapsed. Near the entrance is a trash receptacle turned on its side. What do you do?`,
+    description: `You descend into the dimly lit cave, moving slowly to give your eyes a chance to adjust to the darkness. The walls are lined with slick stones arranged in rectangle patterns. They feel cool against your fingertips. A large crevice in the middle of the cave is dominated by a twisted metal structure that emerges from a tunnel. An ancient home, perhaps? Or was it what the elders called "automobiles"? Whatever its name, it could contain a relic. There is a large pile of rubble to the north where part of the ceiling has collapsed. Near the entrance is a trash receptacle turned on its side. What do you do?`,
 
     choices: [{
 
@@ -24,7 +24,8 @@ const subway = {
 
         result: `You approach the rubble pile and begin sifting through stones. The stones are heavy and you're so distracted with your work that you don't hear the WILD DOG sneak up behind you. It lunges, sinking its teeth deep into your calf! With a yell, you swing wide with a rock in your hand. It connects with the dog's head, dazing it long enough for you to escape. You return to the merchant empty-handed, but alive.`,
         hp: -14,
-        gil: 0
+        gil: 0,
+        tapeChoice: false
     },
     {
         id: 'subwayCar',
@@ -34,9 +35,10 @@ const subway = {
         resultImage: '/recorder1.png',
 
         result: `You enter the mangled structure, the floor beneath you creaking as you step inside. There are cracked and collapsing seats lining either side of the structure. Tucked neatly under a nearby row, you find a wad of fabric. You unfurl it, revealing a moth-eaten blanket protecting a...recorder? And it looks like it still works! Should you press play?`,
-        // tapeChoice here
+
         hp: 0,
-        gil: 0
+        gil: 0,
+        tapeChoice: true
     },
     {
         id: 'trashCan',
@@ -44,7 +46,8 @@ const subway = {
         resultImage: '/phone3.jpg',
         result: `The trash can is full of soggy paper, some strange-colored mold, and a gooey substance you don't think you'll ever get out from under your fingernails. You kick the trash can in frustration and, lo! Underneath it is a small relic! You quickly leave, excited to show the merchant your new find!`,
         hp: 0,
-        gil: 12
+        gil: 12,
+        tapeChoice: false
     }]
 }; 
    
@@ -74,7 +77,8 @@ const mall = {
         result: `Strangely humanoid figures occupy the space, draped in various colored fabrics. Most of what you find is too disintegrated to be useful, but you find a case in the back full of sturdy fabric! The merchant will be so pleased!`,
 
         hp: 0,
-        gil: 10
+        gil: 10,
+        tapeChoice: false
     }, {
         id: 'foodCourt',
 
@@ -85,7 +89,8 @@ const mall = {
         result: `You follow the sweet smell into a cavernous plaza ringed with small shops. There are faded photos of what looks like...food? Nothing that you've ever tasted. The smell is emanating from a shop with a circular logo over the top. You approach the counter and lean over. The ground is littered with what looks like fine, white sand. You're considering jumping over to inspect further when there's a series of loud squeaks nearby. You move quickly, but not quickly enough: soon there's a torrent of fat rats pouring out of every crevice, scrabbling over the counter, and heading straight towards you. You take off running, relics be damned! The merchant will be unhappy but, except for a rough tumble you took on your way out, you've escaped relatively unharmed.`,
 
         hp: -3,
-        gil: 0
+        gil: 0,
+        tapeChoice: false
     }, {
         id: 'pharmacy',
 
@@ -94,9 +99,9 @@ const mall = {
         resultImage: '/recorder1.png',
 
         result: `The elders told you that the plus symbol meant medicine and medicine could definitely come in handy. Most of the aisles here are empty, so you enter the backroom. This place is also cleaned out except for a wad of rotten cloth in the corner and a lonely metal desk set against the wall. You check the drawers and find a recorder! And it still works! Listen to it?`,
-        //tapeChoice
         hp: 0,
-        gil: 0
+        gil: 0,
+        tapeChoice: true
     }]
 }; 
     
@@ -125,9 +130,9 @@ const cabin = {
         resultImage: '/recorder1.png',
 
         result: `You ascend the creaking stairs leading to the loft area, feeling each one bow under your weight. You move slowly, methodically, willing it to hold, and breathe a sigh of relief when you make it to the top. You find an array of useless items, more moth-eaten blankets than you'll ever need to see in an entire lifetime, and a recorder! And it still works? Do you listen?`,
-        //tapeChoice
         hp: 0,
-        gil: 0
+        gil: 0,
+        tapeChoice: true
     }, {
         id: 'well',
 
@@ -138,7 +143,8 @@ const cabin = {
         result: `You lean over the stone wall of the well, looking down, down, down. The rope is long gone, the metal bucket floating in the water thirty feel below. You're wondering if you should go find a container to bring some water back to the merchant when the stone suddenly shifts beneath your palms and you topple forward! You're falling head-first, trying desperately to right yourself when CRACK. Your head connects with the bucket.`,
 
         hp: -50,
-        gil: 0
+        gil: 0,
+        tapeChoice: false
     }, {
         id: 'livingArea',
 
@@ -149,7 +155,8 @@ const cabin = {
         result: `There is a lot of junk here and the ceiling is mostly fallen though. You happen upon a bag in a corner and, after rifling through it, you find two relics! Behind the bag you find something even more useful: an axe! You hurry back to show the merchant. They're going to be so pleased!`,
 
         hp: 0,
-        gil: 20
+        gil: 20,
+        tapeChoice: false
     }]
 };
     
@@ -179,7 +186,8 @@ const groceryStore = {
         result: `Most of the aisles have been cleaned out, but you find a box of some spongey, yellow treat in the very back corner of the shop. You tentatively sniff it, wondering if something could possibly have lasted this long. With a shrug you take a bite and--UGH. GROSS. You throw up everything and are too nauseous to keep searching. You return to the merchant empty-handed and empty-stomached.`,
 
         hp: -8,
-        gil: 0
+        gil: 0,
+        tapeChoice: false
     }, {
         id: 'managerOffice',
         description: 'Explore Upstairs',
@@ -187,9 +195,9 @@ const groceryStore = {
         resultImage: '/recorder1.png',
 
         result: `You creep up the stairs, wondering what kind of overlord sat up here watching the people below. The space is disappointing; there's just a few desks loaded with papers and a depressing figurine with a wobbly head. You search the small trash can beside the desk and find a recorder! And it looks like it works! Do you listen?`,
-        //tapeChoice
         hp: 0,
-        gil: 0
+        gil: 0,
+        tapeChoice: true
     }, {
         id: 'backRoom',
 
@@ -200,7 +208,8 @@ const groceryStore = {
         result: `Most of the boxes are full of food that is long-rotten or nothing of use. But in the corner you find a strange cube-shaped relic! You rush back to the merchant. They're going to be so pleased!`,
 
         hp: 0,
-        gil: 12
+        gil: 12,
+        tapeChoice: false
     }]
 };
     
@@ -230,7 +239,8 @@ const coast = {
         result: `You excitedly romp through the sand, dropping onto your back and rolling around in it. Only  aRG. IT GETS E V E R Y W H E R E.`,
 
         hp: -7,
-        gil: 0
+        gil: 0,
+        tapeChoice: false
     }, {
         id: 'shells',
 
@@ -241,7 +251,8 @@ const coast = {
         result: `You go to look at the tide pool. It's teeming with life! You play with a few snails and fish and end up collecting a bunch of beautiful shells! The merchant will love this!`,
 
         hp: 0,
-        gil: 7
+        gil: 7,
+        tapeChoice: false
     }, {
         id: 'house',
 
@@ -250,9 +261,9 @@ const coast = {
         resultImage: '/recorder1.png',
 
         result: `You enter the house ruins. Things are mostly decayed, except for a few photos of a happy family lining the walls. In the kitchen, you find a recorder on the countertop. It looks like it works! Do you listen?`,
-        //tapeChoice
         hp: 0,
-        gil: 0
+        gil: 0,
+        tapeChoice: true
     }]
 };
 
--- quests/quests.js
+++ quests/quests.js
@@ -1,6 +1,7 @@
 import createChoice from './createChoice.js';
 import { loadProfile, getUser, saveUser, findById } from '../closet.js';
 import quests from '../attic.js';
+import tapes from '../tapeattic.js';
 import scoreQuest from './scoreQuest.js';
 
 loadProfile();
@@ -10,6 +11,8 @@ const searchParams = new URLSearchParams(window.location.search);
 const questId = searchParams.get('id');
 
 const quest = findById(quests, questId);
+//
+const tape = findById(tapes, questId);
 
 if (!quest) {
     window.location = '../map/map.html';
@@ -17,16 +20,20 @@ if (!quest) {
 
 const title = document.getElementById('title');
 const image = document.getElementById('image');
-
 const description = document.getElementById('description');
 const choiceForm = document.getElementById('choice-form');
 const choices = document.getElementById('choices');
+const tapeChoices = document.getElementById('tape-choices');
 const result = document.getElementById('result');
+const tapeResult = document.getElementById('tape-result');
 const resultDescription = document.getElementById('result-description');
+const backButton = document.getElementById('back');
+
 
 title.textContent = quest.title;
 image.src = '../UserQuest' + quest.image;
 description.textContent = quest.description;
+backButton.style.display = 'none';
 
 for (let i = 0; i < quest.choices.length; i++) {
     const choice = quest.choices[i];
@@ -41,13 +48,21 @@ choiceForm.addEventListener('submit', function(e) {
     const choiceId = formData.get('choice');
     const choice = findById(quest.choices, choiceId);
 
+    //
     const user = getUser();
     scoreQuest(choice, quest.id, user);
     saveUser(user);
 
     choiceForm.classList.add('hidden');
+    description.style.display = 'none';
     result.classList.remove('hidden');
+    image.src = '../UserQuest' + choice.resultImage;
     resultDescription.textContent = choice.result;
+    backButton.style.display = 'inline';
 
     loadProfile();
+});
+
+backButton.addEventListener('click', () => {
+    window.location.href = './';
 });
